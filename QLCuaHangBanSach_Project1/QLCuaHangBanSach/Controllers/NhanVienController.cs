9ec3d0411f020d380d1258b755e5272e065c92fb
nguyenvanquy22 <nguyenvanquy9a2003@gmail.com>
2024-10-24 10:48:54 +0700

    add staffs management

---
+++ QLCuaHangBanSach_Project1/QLCuaHangBanSach/Controllers/NhanVienController.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using QLCuaHangBanSach.Models;
+using System.ComponentModel;
+
+namespace QLCuaHangBanSach.Controllers
+{
+    public class NhanVienController : Controller
+    {
+        private DBCuaHangBanSachContext db;
+        public NhanVienController(DBCuaHangBanSachContext _context)
+        {
+            this.db = _context;
+        }
+        public IActionResult Index()
+        {
+            List<Nhanvien> listNhanViens = db.Nhanviens.ToList();
+            return View(listNhanViens);
+        }
+
+        //create
+        public IActionResult ThemNhanVien()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public  IActionResult ThemNhanVien(Nhanvien nhanvien) 
+        {
+            List<Nhanvien> nhanviens = db.Nhanviens.ToList();
+            if (nhanviens.Count > 0)
+            {
+                string tam = nhanviens.Last<Nhanvien>().MaNv.ToString();
+                string[] luu = tam.Split('V');
+                int tam1 = Convert.ToUInt16(luu[1]);
+                tam1 += 1;
+                luu[1] = tam1.ToString();
+                tam = "";
+                luu[0] += "V";
+                for (int i = 0; i < 5 - luu[0].Length - luu[1].Length; i++)
+                {
+                    tam += "0";
+                }
+                nhanvien.MaNv = luu[0] + tam + luu[1];
+            }
+            else
+            {
+                nhanvien.MaNv = "NV001";
+            }
+            if (nhanvien.TenNv != null)
+            {
+                db.Nhanviens.Add(nhanvien);
+                db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View();
+            }  
+        }
+        //sua
+        public IActionResult SuaNhanVien(String id)
+        {
+
+            if (id == null || db.Nhanviens == null)
+            {
+                return NotFound();
+            }
+            var nhanvien = db.Nhanviens.Find(id);
+            if (nhanvien == null)
+            {
+                return NotFound();
+            }
+            return View(nhanvien);
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SuaNhanVien(String id, Nhanvien nhanvien)
+        {
+            if (id != nhanvien.MaNv)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.Update(nhanvien);
+                    db.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NhanVienExists(nhanvien.MaNv))
+                        return NotFound();
+                    else throw;
+
+                }
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(nhanvien);
+        }
+        private bool NhanVienExists(String id)
+        {
+            return( db.Nhanviens?.Any(e => e.MaNv == id)).GetValueOrDefault();
+        }
+
+        //xoa
+        public IActionResult XoaNhanVien(String id)
+        {
+            if (id == null || db.Nhanviens == null)
+            {
+                return NotFound();
+            }
+            var nhanvien = db.Nhanviens.Include(l => l.Hdms)
+            .Include(e => e.Hdbs)
+            .FirstOrDefault(m => m.MaNv == id);
+            if (nhanvien == null)
+            {
+                return NotFound();
+            }
+            if (nhanvien.Hdbs.Count() > 0)
+            {
+                return Content("Không thể xóa!");
+            }
+            return View(nhanvien);
+        }
+        [HttpPost, ActionName("XoaNhanVien")]
+        [ValidateAntiForgeryToken]
+        public IActionResult XacNhanXoa(String id)
+        {
+            if (db.Nhanviens == null)
+            {
+                return Problem("Không có nhân viên!");
+            }
+            var learner = db.Nhanviens.Find(id);
+            if (learner != null)
+            {
+                db.Nhanviens.Remove(learner);
+            }
+            db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
